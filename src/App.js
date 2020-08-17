fc472bb9ef3287a99645d0788765754743f502f6
HexerMelvin117 <melvinre1997@gmail.com>
2020-08-17 00:18:13 -0600

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   const [items, setItems] = React.useState([]);
@@ -23,22 +29,33 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>TODOs</h1>
+    <Router>
+      <Link exact to="/">Home</Link>
+      <Link to="/second">Second Page</Link>
+      <Switch>
+        <Route path="/second">
+          <h1>Second Page Placeholder</h1>
+        </Route>
+        <Route path="/">
+          <div>
+            <h1>TODOs</h1>
 
-      <ul>
-        {items.map((item) => (
-          <li key={item.id}>{item.text}</li>
-        ))}
-      </ul>
+            <ul>
+              {items.map((item) => (
+                <li key={item.id}>{item.text}</li>
+              ))}
+            </ul>
 
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="new-todo">What needs to be done?</label>
-        <br />
-        <input id="new-todo" value={text} onChange={handleChange} />
-        <button>Add #{items.length + 1}</button>
-      </form>
-    </div>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="new-todo">What needs to be done?</label>
+              <br />
+              <input id="new-todo" value={text} onChange={handleChange} />
+              <button>Add #{items.length + 1}</button>
+            </form>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
