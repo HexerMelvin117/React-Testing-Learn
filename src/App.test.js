d883ba2a044e1cc3fd109dd85370de5fa2a0c663
HexerMelvin117 <melvinre1997@gmail.com>
2020-08-17 00:05:28 -0600

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import * as React from 'react';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+test('Renders needed content', () => {
+  const { getByText, getByLabelText } = render(<App />)
+
+  getByText("TODOs");
+  getByLabelText("What needs to be done?");
+  getByText("Add #1");
 });
+
+test("allows users to add items", () => {
+  const { getByText, getByLabelText } = render(<App />)
+
+  const input = getByLabelText("What needs to be done?")
+  const button = getByText("Add #1")
+
+  userEvent.type(input, "Learn german")
+  userEvent.click(button)
+
+  getByText("Learn german");
+  getByText("Add #2")
+})
